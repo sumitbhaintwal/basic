6c59eba14fc7bfe2558796429b3ea99f4de5fac7
Sumit Bhaintwal <sumitsharma4ever@gmail.com>
2016-04-25 13:56:25 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/reset.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery-ui.min.css">
+</head>
+<body>
+
